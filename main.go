032d371fe9e9a724440e0289156d6ad5eac443be
ßUG5Y <105293349+BUG5Y@users.noreply.github.com>
2023-12-01 16:52:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -25,18 +25,49 @@ import (
 // Function to pause the script. Ask if the found URLs are good and okay to continue. (y/n) If we say no then it will wait till we adjust the file and say to continue(c)
 
 // Main loop (Casting a big net)
+func bigNet() {
+// Start with a list of known URLs & Company names
+// Whoxyrm with the company name(s) add into the known URLs
+// Have script pause and have us check if all URLs are in target. y/n. If bad urls remove the bad URLs from the file and then continue.
+// ASN lookup with company names. Create a file of ASNs
+// Pull domains and IPs with ASN, add to URLs and a create a IP file
+// Have script pause and check if all URLs are in target. y/n. If not remove the bad URLs from the file and then continue.
+// Use findomain
+// Use assetfinder
+// Use host command to find sites are pointed to
+// Remove sites that are pointed to akima, amazon, etc.
+// Use httpx using the asn flag and webserver flag. Add asn’s to file
+// Sort 400 type codes to a new file
+// If loop has not been completed go back to step 4
+// If scope is strict remove all URLs that do not strictly adhere
+
+}
 
 // Zoning in round 1
+func webEnum () {
+}
 
 // Zoning in round 2
+func ipEnum () {
+}
 
 // Function to parse ASNs
-
+func parseASN (){
+}
 // Function to parse IPs and IP Ranges
+func parseIP (){
+}
+
+func parseRange (){
+}
 
 // Function to parse status codes
+func parseStatus (){
+}
 
 // Function to track progress % or active run time
+func trackProg (){
+}
 
 func main() {
     runCommand(currentFunction(), "ping", "-c1", "google.commm")
